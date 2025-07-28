185d191761a51bda750fbb8f85350c1b68b58db3
bayronDu <bayronromero9@gmail.com>
2025-07-28 13:00:31 -0500

    feat: :hammer: Rick and Morty appi

---
+++ dia5/scripts/scripts.js
@@ -0,0 +1,17 @@
+const consulta = new XMLHttpRequest();
+let inicio = Number(prompt("Por favor digite el ID del personaje a consultar"));
+consulta.open('GET','https://rickandmortyapi.com/api/character/'+`${inicio}`, true);
+consulta.onload = function () {
+    if (consulta.status >= 200 && consulta.status < 300) {
+        const data = JSON.parse(consulta.responseText);
+        console.log(data);
+        alert(data);
+    } else {
+        console.error('Error:', consulta.status, consulta.statusText);
+    }
+};
+consulta.onerror = function(){
+    console.error('Error de red');
+};
+
+consulta.send();
